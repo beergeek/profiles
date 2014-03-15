4209f6cb7627c6a17fd058e43e96edecf653ca46
beergeek <brett.gray@puppetlabs.com>
2014-03-16 03:24:05 +1100

    added more stuff

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,18 +1,9 @@
 class profiles::base {
 
-  #$sysctl_settings = hiera('profiles::base::sysctl_settings')
-  ##$sysctl_defaults = hiera('profiles::base::sysctl_defaults')
+  $sysctl_settings = hiera('profiles::base::sysctl_settings')
+  $sysctl_defaults = hiera('profiles::base::sysctl_defaults')
 
-  #create_resources(sysctl,$sysctl_settings, $sysctl_defaults)
+  create_resources(sysctl,$sysctl_settings, $sysctl_defaults)
 
-  if $::osfamily != 'windows' {
-    class {'ntp':
-      servers => ['192.168.61.1'],
-    }
-  } else {
-    class {'winntp':
-      ntp_servers => ['192.168.61.1'],
-    }
-  }
-
+  include profiles::ntp_client
 }
